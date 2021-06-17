88963615209206ceb80cc02762ce36a0b93683ca
Karim <karim.asu92@gmail.com>
2021-06-17 20:44:06 +0200

    Handled errors when image unavailable from the marvel api.

--- app/src/main/java/com/karim/marveldemo/binding/ViewBinding.kt
+++ app/src/main/java/com/karim/marveldemo/binding/ViewBinding.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.karim.marveldemo.R
 
 object ViewBinding {
 
@@ -12,6 +13,7 @@ object ViewBinding {
     fun bindLoadImage(view: AppCompatImageView, url: String) {
         Glide.with(view.context)
             .load(url)
+            .error(R.drawable.image_unavailable)
             .into(view)
     }
 
